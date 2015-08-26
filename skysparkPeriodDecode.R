d9d994f178764dc8ff7aa152bfe19bcf7a23d27b
Brian Sullivan <bsull184@gmail.com>
2015-08-26 16:10:15 -0400

    initial commit

---
+++ skysparkPeriodDecode.R
@@ -0,0 +1,29 @@
+
+##function converts a character to its index position in base64
+fromBase64 <- function(character){
+  base64 <- unlist(strsplit("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/", ""))
+  which(base64 ==  character) - 1
+}
+
+## function takes a "periods" string from skyspark sparks() result and converts it to a data frame of all times
+## current returns in the form of a data frame with starting time in col1, duration in col2
+## all times are in minutes
+decodePeriod <- function(periodString){
+  characters <- unlist(strsplit(periodString, ""))
+  characters <- as.numeric(sapply(characters, fromBase64))
+  
+  results <- data.frame(startTime = integer(), duration = integer())
+  i <- 1
+    
+  while (i < length(characters)){
+    time <- bitwShiftL(characters[i], 6) + characters[i + 1]
+    duration <- bitwShiftL(characters[i + 2], 6) + characters[i + 3]
+    
+    results[(i-1)/4 + 1,] <- c(time, duration)
+    
+    i <- i + 4
+  }
+
+  return(results)
+}
+
