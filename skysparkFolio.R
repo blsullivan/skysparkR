b385d9f94639e389c17da9741e98fc843491b291
Brian Sullivan <bsull184@gmail.com>
2015-08-26 16:01:39 -0400

    updated file support

--- skysparkFolio.R
+++ skysparkFolio.R
@@ -1,18 +1,37 @@
 library(httr)
 
-GetSkysparkFolio <- function(expr, cookie, server, project){
+GetSkysparkFolio <- function(expr, cookie, server, project, datatype){
   #expr is an Axon code chunk.  Any quotes used in expr must be properly escaped.
   #e.g. expr <- "read(point and dis == \"Point Name\")" 
   #cookie needs to be generated from the function GetSkysparkCookie(username, password, server, project)
   #sever url should not contain / at the end
   #returns the results of the Folio/Axon code chunk in CSV format.  Return format can be changed to zinc, JSON, or xml by modifying headers
+  #datatype = "json", "zinc", "csv"
   
   apiUrl <- paste0(server, "/api/", project, "/eval")
   
   requestBody <- paste0("ver: \"2.0\"\nexpr\n\"", expr, "\"")
   
-  POST(url = apiUrl,
-       config = add_headers("Content-Type" = "text/plain", "charset" = "utf-8", "Accept" = "text/csv", "Cookie" = cookie),
-       body = requestBody,
-       encode = "multipart")
+  if (datatype == "json"){
+    return(POST(url = apiUrl,
+                config = add_headers("Content-Type" = "text/plain", "charset" = "utf-8", "Cookie" = cookie, "Accept" = "application/json"),
+                body = requestBody,
+                encode = "multipart"))
+  }
+  
+  if (datatype == "csv"){
+    return(POST(url = apiUrl,
+                config = add_headers("Content-Type" = "text/plain", "charset" = "utf-8", "Cookie" = cookie, "Accept" = "text/csv"),
+                body = requestBody,
+                encode = "multipart"))
+  }
+  
+  if (datatype == "zinc"){
+    return(POST(url = apiUrl,
+                config = add_headers("Content-Type" = "text/zinc", "charset" = "utf-8", "Cookie" = cookie),
+                body = requestBody,
+                encode = "multipart"))
+  } 
+  
+  
 }
